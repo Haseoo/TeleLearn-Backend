e264cb4c4c9351c4b1423a4d1799243dd8c52441
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-11-21 14:45:10 +0100

    Fix requests

--- src/main/java/kielce/tu/weaii/telelearn/requests/courses/ScheduleTaskRequest.java
+++ src/main/java/kielce/tu/weaii/telelearn/requests/courses/ScheduleTaskRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
@@ -16,13 +17,14 @@ public class ScheduleTaskRequest {
     private Long taskId;
     private Long studentId;
     private Duration plannedTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
     @Valid
     @JsonCreator
-    public ScheduleTaskRequest(Long taskId,
-                               Long studentId,
-                               LocalDate date,
+    public ScheduleTaskRequest(@JsonProperty(value = "taskId", required = true) Long taskId,
+                               @JsonProperty(value = "studentId", required = true)Long studentId,
+                               @JsonProperty(value = "date", required = true) LocalDate date,
                                @Min(value = 0, message = "Nieprawidłowa liczba godzin") @JsonProperty(value = "hours", required = true) long hours,
                                @Range(min = 0, max = 60, message = "Nieprawidłowa liczba minut") @JsonProperty(value = "minutes", required = true) long minutes) {
         this.taskId = taskId;
